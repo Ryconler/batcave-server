718bc041bdfeb33a9059927223edda5686b8eb1f
Ryconler <1290279000@qq.com>
2019-03-09 15:47:43 +0800

    'b'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,6 @@ module.exports = {
     apps: [{
         name: 'batcave-server',
         script: 'bin/www',
-        autorestart: true,
-        watch: true,
         env: {
             NODE_ENV: 'production'
         },
