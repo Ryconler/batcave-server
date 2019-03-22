c13e6c2bb5ec8ed18b3edb066433c8acd548e822
Ryconler <1290279000@qq.com>
2019-03-22 18:14:01 +0800

    文件不能下载

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
     apps: [{
         name: 'batcave-server',
         script: 'bin/www',
+        autorestart: true,
+        watch: true,
         env: {
             NODE_ENV: 'production'
         },
--- middlewines/uploadMulter.js
+++ middlewines/uploadMulter.js
@@ -1,7 +1,11 @@
 const multer  = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        if (process.env.NODE_ENV === 'production') {
+            cb(null, 'C:/Users/Administrator/Desktop/batcave/uploads')
+        }else {
+            cb(null, 'uploads')
+        }
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname)
