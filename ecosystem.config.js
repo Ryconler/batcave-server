f3ac61ad7aca6e78c061c0c50dc9b8a222ec89ae
Ryconler <1290279000@qq.com>
2019-04-25 10:37:41 +0800

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: 'batcave-server',
         script: 'bin/www',
         watch: true,
-        ignore_watch: ['uploads'],
+        ignore_watch: ['uploads','node_modules'],
         env: {
             NODE_ENV: 'production'
         },
