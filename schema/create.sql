309beeb69fe1a38f27cd69f5acd2433fc4f3d223
朱星杰 <1290279000@qq.com>
2020-07-17 23:34:46 +0800

    sql更新

--- schema/create.sql
+++ schema/create.sql
@@ -87,3 +87,24 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+INSERT INTO `batcave`.`file`(`id`, `uid`, `title`, `location`, `type`, `describe`, `private`, `create_date`) VALUES (1, 1, '历代蝙蝠侠头像', '1594999809416-IMG_0154.JPG', '图片', '', '0', '2020-07-17 23:30:09');
+INSERT INTO `batcave`.`file`(`id`, `uid`, `title`, `location`, `type`, `describe`, `private`, `create_date`) VALUES (2, 1, '历代小丑头像', '1594999854764-IMG_0153.JPG', '图片', '', '0', '2020-07-17 23:30:54');
+
+INSERT INTO `batcave`.`like`(`id`, `uid`, `rid`, `fid`, `like_date`) VALUES (1, 1, 1, NULL, '2020-07-17 23:22:04');
+INSERT INTO `batcave`.`like`(`id`, `uid`, `rid`, `fid`, `like_date`) VALUES (2, 1, 2, NULL, '2020-07-17 23:22:30');
+INSERT INTO `batcave`.`like`(`id`, `uid`, `rid`, `fid`, `like_date`) VALUES (3, 1, 3, NULL, '2020-07-17 23:22:55');
+INSERT INTO `batcave`.`like`(`id`, `uid`, `rid`, `fid`, `like_date`) VALUES (4, 1, NULL, 1, '2020-07-17 23:30:11');
+INSERT INTO `batcave`.`like`(`id`, `uid`, `rid`, `fid`, `like_date`) VALUES (5, 1, NULL, 2, '2020-07-17 23:31:19');
+
+
+INSERT INTO `batcave`.`record`(`id`, `content`, `username`, `create_date`) VALUES (1, 'hello', 'zhuxingjie', '2020-07-17 23:11:51');
+
+
+INSERT INTO `batcave`.`url`(`id`, `uid`, `title`, `content`, `type`, `create_date`) VALUES (1, 1, '个人Demo - 蝙蝠洞 Batcave网站', 'http://batcave.wzmxx.com', '官网', '2020-07-17 23:22:02');
+INSERT INTO `batcave`.`url`(`id`, `uid`, `title`, `content`, `type`, `create_date`) VALUES (2, 1, '个人首页 - JesseZhu', 'http://www.wzmxx.com', '官网', '2020-07-17 23:22:29');
+INSERT INTO `batcave`.`url`(`id`, `uid`, `title`, `content`, `type`, `create_date`) VALUES (3, 1, '毕设作品 - 用户行为分析平台', 'http://analytics.wzmxx.com', '官网', '2020-07-17 23:22:54');
+
+
+INSERT INTO `batcave`.`user`(`id`, `username`, `password`, `tmp_password`, `email`, `register_date`) VALUES (1, 'zhuxingjie', '$2a$10$7rltTnVt1UXOJlYiMnsSluOwpNcrOwreYp7fsRJ8n5tWSxDRtiUbu', '', '123@qq.com', '2020-07-17 23:00:07');
