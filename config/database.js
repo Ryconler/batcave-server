f5178e47cab3e82ea140afe8e814762fecf643e8
zhuxingjie <zhuxingjie@chances.com.cn>
2021-01-07 17:20:21 +0800

    配置修改

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports={
     database:'batcave',
-    username:'root',
-    password:'root',
+    username:process.env.DATABASE_USERNAME || 'root',
+    password:process.env.DATABASE_PASSWORD || 'root',
     host:'127.0.0.1',
     port:3306
 }
--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const devFlag = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     devFlag,
-    baseURL: devFlag ? 'http://localhost:8080' : 'http://batcave.jessezhu.cn',
+    baseURL: devFlag ? 'http://localhost:8080' : process.env.CORS_ORIGIN,
     port: 5000
 }
