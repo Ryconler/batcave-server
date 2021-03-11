ad6dae075985b0f470227effde664a230347a07a
朱星杰 <1290279000@qq.com>
2021-03-11 22:00:20 +0800

    开发环境跨域不限制

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const devFlag = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     devFlag,
-    baseURL: devFlag ? 'http://localhost:8080' : process.env.CORS_ORIGIN,
+    baseURL: devFlag ? '*' : process.env.CORS_ORIGIN,
     port: 5000
 }
